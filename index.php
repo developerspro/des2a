6e42c943eaa78964cc41c6c7221a5245d0fff499
Jeferson Ferreira <jeferson.oferreria@etec.sp.gov.br>
2022-08-09 17:53:01 -0300

    alterações

--- index.php
+++ index.php
@@ -16,17 +16,35 @@
             <form action="principal.php" method="POST">
                 <div class="mb-3">
                     <label for="form-usuario" class="form-label">Usuario</label>
-                    <input type="text" class="form-control" id="usuario" placeholder="digite seu usuario">
+                    <input type="text" class="form-control" name="usuario" id="usuario" placeholder="digite seu usuario">
                 </div>
                 <div class="mb-3">
                     <label for="exampleFormControlTextarea1" class="form-label">Senha</label>
-                    <input type="text" class="form-control" id="senha">
+                    <input type="text" class="form-control" name="senha" id="senha">
                 </div>
+                <div class="mb-3">
+                <button class="btn btn-dark" type="submit">Enviar</button>
+                <button class="btn btn-danger" type="reset">Limpar</button>
+                </div>
+               
             </form>
         </div>
         <div class="col"></div>
     </div>
 
+
+    <div class="container text-center">
+  <div class="row">
+    <div class="col">col</div>
+    <div class="col">col</div>
+    <div class="col">col</div>
+    <div class="col">col</div>
+  </div>
+  <div class="row">
+    <div class="col-8">col-8</div>
+    <div class="col-4">col-4</div>
+  </div>
+</div>
 </body>
 
 </html>
